bf1dea657e1b777046836f0c305fd39eb4970d1b
khanh-ch-ny <khanh040605@gmail.com>
2024-12-06 16:39:58 +0700

    add file lesson

---
+++ lesson 1/built-in.js
@@ -0,0 +1,14 @@
+console.log('khanh')
+
+confirm('khanh dep trai')
+
+prompt('khanh co dep trai khong')
+
+setTimeout(function(){
+    alert('thong bao')
+
+} , 1000)
+
+setInterval(function()  {
+    console.log('thong bao' + Math.random() )
+}, 1000);
